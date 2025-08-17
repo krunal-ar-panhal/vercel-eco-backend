1cbe4f431d08bd827b5ffe2fc204b65442843567
krunal-ar-panhal <krunalapnchalkp2123@gmail.com>
2025-08-18 01:34:42 +0530

    change

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config()
 const PORT = process.env.PORT || 4000;
 
 const corsOptions = {
-  origin: ['http://localhost:5174', 'http://localhost:5173'], 
+  origin: ['https://vercel-eco-frontend.vercel.app/'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'token'],
   credentials: true, 
